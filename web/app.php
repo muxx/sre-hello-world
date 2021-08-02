3af83fc5d35fb64e165cdca267513c55f12fc71d
Ilyas Salikhov <salikhoff@gmail.com>
2021-08-02 15:29:34 +0300

    Added css and image to app.php

--- web/app.php
+++ web/app.php
@@ -20,18 +20,35 @@ $env = $_ENV;
 ksort($env);
 
 ?>
+<html>
+<head>
+    <title>SRE Hello World</title>
+    <style>
+        img {
+            width: 300px;
+            height: auto;
+            display: block;
+        }
+        body {
+            font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif;
+            line-height: 1.5;
+        }
+        pre, code {
+            font-family: SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace;
+        }
+    </style>
+</head>
+<body>
+<img src="title.jpg" alt="SRE Hello World" />
+<h1>Data from the table <code>`author`</code></h1>
 <pre>
-Data from table `author`
-
 ===|======
 id | name
 ---|------
 <?php foreach ($result as $row) { echo " {$row['id']} | {$row['name']}\n"; } ?>
-==========
-
----------------------------------------------------------------------------------
-
-Debug of ENV vars
-==================
+</pre>
+<h1>Debug of ENV vars</h1>
+<pre>
 <?php foreach ($env as $key => $value) { echo "$key=$value\n"; } ?>
 </pre>
+</body>
